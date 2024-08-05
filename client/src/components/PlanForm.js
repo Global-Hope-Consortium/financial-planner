0a26a2ffe263ea37956d7f9c6aee899dc0a547b6
Immaculate Nyoni <sthwaloe@gmail.com>
2024-08-05 19:34:24 +0200

    Commit before ejecting Create React App

--- client/src/components/PlanForm.js
+++ client/src/components/PlanForm.js
@@ -1,9 +1,3 @@
-import Home from './components/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Affirmations from './components/Affirmations';
-import Plans from './components/Plans';
-import UnifiedView from './components/UnifiedView';
-import Navbar from './components/Navbar';
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -56,18 +50,4 @@ const PlanForm = () => {
   );
 };
 
-const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/plans" element={<Plans />} />
-        <Route path="/affirmations" element={<Affirmations />} />
-        <Route path="/unified-view" element={<UnifiedView />} />
-      </Routes>
-    </Router>
-  );
-};
-
-export default App;
+export default PlanForm;
